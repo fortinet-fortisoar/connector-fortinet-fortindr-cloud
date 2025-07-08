28b11d12dd833adabd903e9ab567d7a58fa279e5
Smorankar <smorankar@shuklaamacOS 15.0.1 Shukla's Fortinet MacBook ProAbhishek Shukla>
2025-07-08 19:48:14 +0530

    Bug Fixed

--- fortinet-fortindr-cloud/operations.py
+++ fortinet-fortindr-cloud/operations.py
@@ -1,5 +1,5 @@
 """ Copyright start
-  Copyright (C) 2008 - 2023 Fortinet Inc.
+  Copyright (C) 2008 - 2025 Fortinet Inc.
   All rights reserved.
   FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
   Copyright end """
@@ -7,8 +7,12 @@
 import requests, json, os
 from django.conf import settings
 from connectors.core.connector import ConnectorError, get_logger
-from connectors.cyops_utilities.builtins import upload_file_to_cyops
 from .constants import *
+
+try:
+    from connectors.cyops_utilities.builtins import upload_file_to_cyops
+except:
+    pass
 
 logger = get_logger('fortinet-fortindr-cloud')
 
---
+++ fortinet-fortindr-cloud/release_notes.md
@@ -0,0 +1,3 @@
+#### The following enhancements have been made to the Fortinet FortiNDR Cloud connector in version 1.1.0:
+
+- The issue with data ingestion has been resolved.
