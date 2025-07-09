8ab574abb27d772d6bc4dc14b85c6d8affab617f
Smorankar <smorankar@shuklaamacOS 15.0.1 Shukla's Fortinet MacBook ProAbhishek Shukla>
2025-07-09 16:21:39 +0530

    Updated Copyright

--- fortinet-fortindr-cloud/operations.py
+++ fortinet-fortindr-cloud/operations.py
@@ -1,8 +1,9 @@
-""" Copyright start
-  Copyright (C) 2008 - 2025 Fortinet Inc.
-  All rights reserved.
-  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
-  Copyright end """
+"""
+Copyright start
+MIT License
+Copyright (c) 2025 Fortinet Inc
+Copyright end
+"""
 
 import requests, json, os
 from django.conf import settings
